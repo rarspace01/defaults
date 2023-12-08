a1b5e88957aef7a314d5e69f200c4e162d0afa5a
rarspace01 <rarspace01@users.noreply.github.com>
2023-12-08 14:27:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,3 +29,19 @@ flatpak install com.tomjwatson.Emote
 #install sdkman for jdk
 curl -s "https://get.sdkman.io" | bash
 sdk install java 21.0.1-tem
+
+# docker
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
