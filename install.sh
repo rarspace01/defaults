f7d5917faf0370c06c6c0914702de9557455efb1
rarspace01 <rarspace01@users.noreply.github.com>
2023-12-08 08:37:34 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt install htop nload git tmux zsh curl wget
+# install oh my zshell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install power 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
