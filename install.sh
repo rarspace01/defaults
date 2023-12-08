b570da18bf396ac43c2a50e9beada23cf0ddec1c
rarspace01 <rarspace01@users.noreply.github.com>
2023-12-08 08:38:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ sudo apt update && sudo apt install htop nload git tmux zsh curl wget
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # install power 10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# install rclone
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+
